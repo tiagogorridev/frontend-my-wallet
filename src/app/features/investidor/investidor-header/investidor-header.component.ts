f4b8d64e392e9365e429fc0cb28c2a02a33e9131
Tiago Gorri <155651809+tiagogorridev@users.noreply.github.com>
2025-04-01 10:17:13 -0300

    feat: implementação da lógica de modal para adicionar ativo
    - Adicionada lógica para abrir e fechar o modal com o método `openModal()` e `closeModal()`.
    - Implementado formulário reativo com `FormGroup` para capturar dados de tipo de ativo, nome, preço, data de compra, quantidade e valor comprado.
    - Inclusão de validações para garantir que os campos do formulário sejam preenchidos corretamente antes do envio.
    - Implementada a funcionalidade de resetar o formulário ao fechar o modal.
    - Adicionada escuta para a tecla `Escape` para fechar o modal quando pressionada.

--- src/app/features/investidor/investidor-header/investidor-header.component.ts
+++ src/app/features/investidor/investidor-header/investidor-header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, HostListener } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-investidor-header',
@@ -6,5 +7,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./investidor-header.component.scss']
 })
 export class InvestidorHeaderComponent {
+  isModalOpen = false;
+  assetForm!: FormGroup;
 
+  assetTypes = [
+    { value: 'crypto', label: 'Criptomoeda' },
+    { value: 'fixed-income', label: 'Renda Fixa' },
+    { value: 'stocks', label: 'Ações' },
+    { value: 'fiis', label: 'FIIS' }
+  ];
+
+  openModal() {
+    this.isModalOpen = true;
+    console.log('Modal opened');
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+    this.assetForm.reset();
+  }
+
+  onSubmit() {
+    if (this.assetForm.valid) {
+      console.log(this.assetForm.value);
+      this.closeModal();
+    } else {
+      Object.keys(this.assetForm.controls).forEach(field => {
+        const control = this.assetForm.get(field);
+        control?.markAsTouched();
+      });
+    }
+  }
+
+  @HostListener('document:keydown.escape', ['$event'])
+  handleEscapeKey(event: KeyboardEvent) {
+    if (this.isModalOpen) {
+      this.closeModal();
+    }
+  }
 }
